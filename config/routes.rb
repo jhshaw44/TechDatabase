ed4bcbf10d93e8cf328d23676c88d923e11ae619
Gene Burchette <eburch2@umbc.edu>
2016-07-18 19:43:33 -0400

    levan's changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+
+  root 'data#index'
+  resources :data
+
+  resources :schools
+
+  get 'welcome/homepage'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  
+  #root 'welcome#homepage'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
