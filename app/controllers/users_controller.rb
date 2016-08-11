75c9eccb87cdaf858826f0390d2466dc49b1d4d8
kgapulan <kgapul1@students.towson.edu>
2016-08-11 09:46:25 -0400

    cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,4 @@ end
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
-  
-  
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   resources :sessions
- # get '/login' =>'sessions#new'
   post 'login' => 'sessions#create'
 
   
