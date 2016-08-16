c2cc16ee778379007637cea6bd9c694adc624e63
kgapulan <kgapul1@students.towson.edu>
2016-08-16 10:39:43 -0400

    -fixed log in function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
   
   def create
-  @user = User.find_by_email(params[:email])
-  if @user && @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect_to root_url, notice: "Logged in!"
+  user= User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+   redirect_to root_url, notice: "Logged in!"
   else
     flash.now.alert = "Email or password is invalid."
     end 
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   get 'signup'=> 'users#new'
   resources :users 
   
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#new", :as => "log_in"
+ 
+  get "/log_in" => "sessions#new", :as => "log_in"
+  post '/login' => 'sessions#create'
+  get "/log_out" => "sessions#destroy", :as => "log_out"
   resources :sessions
-  post 'login' => 'sessions#create'
-
   
 
 end
